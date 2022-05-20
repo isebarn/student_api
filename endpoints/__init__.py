2d098b7abe430fb20fa8f5609507475cf4dbba91
isebarn182@gmail.com <isebarn182@gmail.com>
2022-05-20 19:59:49 +0100

    Wow

--- endpoints/__init__.py
+++ endpoints/__init__.py
@@ -20,6 +20,7 @@ from flask_restx.fields import Raw
 # Local application imports
 import models
 
+
 class Resource(_Resource):
     dispatch_requests = []
 
@@ -35,225 +36,227 @@ class Resource(_Resource):
 
         return super(Resource, self).dispatch_request(*args, **kwargs)
 
+
 api = Namespace("api", description="")
-program_base = api.model('program_base', models.Program.base())
-program_reference = api.model('program_reference', models.Program.reference())
-program_full = api.model('program', models.Program.model(api))
-airport_code_base = api.model('airport_code_base', models.AirportCode.base())
-airport_code_reference = api.model('airport_code_reference', models.AirportCode.reference())
-airport_code_full = api.model('airport_code', models.AirportCode.model(api))
-flight_info_base = api.model('flight_info_base', models.FlightInfo.base())
-flight_info_reference = api.model('flight_info_reference', models.FlightInfo.reference())
-flight_info_full = api.model('flight_info', models.FlightInfo.model(api))
-host_family_base = api.model('host_family_base', models.HostFamily.base())
-host_family_reference = api.model('host_family_reference', models.HostFamily.reference())
-host_family_full = api.model('host_family', models.HostFamily.model(api))
-account_base = api.model('account_base', models.Account.base())
-account_reference = api.model('account_reference', models.Account.reference())
-account_full = api.model('account', models.Account.model(api))
-student_personal_data_base = api.model('student_personal_data_base', models.StudentPersonalData.base())
-student_personal_data_reference = api.model('student_personal_data_reference', models.StudentPersonalData.reference())
-student_personal_data_full = api.model('student_personal_data', models.StudentPersonalData.model(api))
+program_base = api.model("program_base", models.Program.base())
+program_reference = api.model("program_reference", models.Program.reference())
+program_full = api.model("program", models.Program.model(api))
+airport_base = api.model("airport_base", models.Airport.base())
+airport_reference = api.model("airport_reference", models.Airport.reference())
+airport_full = api.model("airport", models.Airport.model(api))
+flight_info_base = api.model("flight_info_base", models.FlightInfo.base())
+flight_info_reference = api.model(
+    "flight_info_reference", models.FlightInfo.reference()
+)
+flight_info_full = api.model("flight_info", models.FlightInfo.model(api))
+host_family_base = api.model("host_family_base", models.HostFamily.base())
+host_family_reference = api.model(
+    "host_family_reference", models.HostFamily.reference()
+)
+host_family_full = api.model("host_family", models.HostFamily.model(api))
+account_base = api.model("account_base", models.Account.base())
+account_reference = api.model("account_reference", models.Account.reference())
+account_full = api.model("account", models.Account.model(api))
+student_personal_data_base = api.model(
+    "student_personal_data_base", models.StudentPersonalData.base()
+)
+student_personal_data_reference = api.model(
+    "student_personal_data_reference", models.StudentPersonalData.reference()
+)
+student_personal_data_full = api.model(
+    "student_personal_data", models.StudentPersonalData.model(api)
+)
 
 
 @api.route("/program")
 class ProgramController(Resource):
-
-    @api.marshal_list_with(api.models.get('program'), skip_none=True)
+    @api.marshal_list_with(api.models.get("program"), skip_none=True)
     def get(self):
         return models.Program.fetch(request.args)
 
-    @api.marshal_with(api.models.get('program'))
+    @api.marshal_with(api.models.get("program"))
     def post(self):
         return models.Program(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('program'))
+    @api.marshal_with(api.models.get("program"))
     def put(self):
         data = models.Program(**request.get_json()).save()
         return data.to_json()
 
-    @api.marshal_with(api.models.get('program'))
+    @api.marshal_with(api.models.get("program"))
     def patch(self):
         return models.Program.set(**request.get_json()).to_json()
 
 
 @api.route("/program/<program_id>")
 class BaseProgramController(Resource):
-
     def delete(self, program_id):
         return models.Program.get(id=program_id).delete()
 
 
 @api.route("/program/base")
 class BaseProgramController(Resource):
-
-    @api.marshal_list_with(api.models.get('program_reference'))
+    @api.marshal_list_with(api.models.get("program_reference"))
     def get(self):
         return [x.to_json(False) for x in models.Program.get(**request.args)]
 
-@api.route("/airport_code")
-class AirportCodeController(Resource):
 
-    @api.marshal_list_with(api.models.get('airport_code'), skip_none=True)
+@api.route("/airport")
+class AirportController(Resource):
+    @api.marshal_list_with(api.models.get("airport"), skip_none=True)
     def get(self):
-        return models.AirportCode.fetch(request.args)
+        return models.Airport.fetch(request.args)
 
-    @api.marshal_with(api.models.get('airport_code'))
+    @api.marshal_with(api.models.get("airport"))
     def post(self):
-        return models.AirportCode(**request.get_json()).to_json()
+        return models.Airport(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('airport_code'))
+    @api.marshal_with(api.models.get("airport"))
     def put(self):
-        data = models.AirportCode(**request.get_json()).save()
+        data = models.Airport(**request.get_json()).save()
         return data.to_json()
 
-    @api.marshal_with(api.models.get('airport_code'))
+    @api.marshal_with(api.models.get("airport"))
     def patch(self):
-        return models.AirportCode.set(**request.get_json()).to_json()
+        return models.Airport.set(**request.get_json()).to_json()
 
 
-@api.route("/airport_code/<airport_code_id>")
-class BaseAirportCodeController(Resource):
+@api.route("/airport/<airport_id>")
+class BaseAirportController(Resource):
+    def delete(self, airport_id):
+        return models.Airport.get(id=airport_id).delete()
 
-    def delete(self, airport_code_id):
-        return models.AirportCode.get(id=airport_code_id).delete()
 
-
-@api.route("/airport_code/base")
-class BaseAirportCodeController(Resource):
-
-    @api.marshal_list_with(api.models.get('airport_code_reference'))
+@api.route("/airport/base")
+class BaseAirportController(Resource):
+    @api.marshal_list_with(api.models.get("airport_reference"))
     def get(self):
-        return [x.to_json(False) for x in models.AirportCode.get(**request.args)]
+        return [x.to_json(False) for x in models.Airport.get(**request.args)]
+
 
 @api.route("/flight_info")
 class FlightInfoController(Resource):
-
-    @api.marshal_list_with(api.models.get('flight_info'), skip_none=True)
+    @api.marshal_list_with(api.models.get("flight_info"), skip_none=True)
     def get(self):
         return models.FlightInfo.fetch(request.args)
 
-    @api.marshal_with(api.models.get('flight_info'))
+    @api.marshal_with(api.models.get("flight_info"))
     def post(self):
         return models.FlightInfo(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('flight_info'))
+    @api.marshal_with(api.models.get("flight_info"))
     def put(self):
         data = models.FlightInfo(**request.get_json()).save()
         return data.to_json()
 
-    @api.marshal_with(api.models.get('flight_info'))
+    @api.marshal_with(api.models.get("flight_info"))
     def patch(self):
         return models.FlightInfo.set(**request.get_json()).to_json()
 
 
 @api.route("/flight_info/<flight_info_id>")
 class BaseFlightInfoController(Resource):
-
     def delete(self, flight_info_id):
         return models.FlightInfo.get(id=flight_info_id).delete()
 
 
 @api.route("/flight_info/base")
 class BaseFlightInfoController(Resource):
-
-    @api.marshal_list_with(api.models.get('flight_info_reference'))
+    @api.marshal_list_with(api.models.get("flight_info_reference"))
     def get(self):
         return [x.to_json(False) for x in models.FlightInfo.get(**request.args)]
 
+
 @api.route("/host_family")
 class HostFamilyController(Resource):
-
-    @api.marshal_list_with(api.models.get('host_family'), skip_none=True)
+    @api.marshal_list_with(api.models.get("host_family"), skip_none=True)
     def get(self):
         return models.HostFamily.fetch(request.args)
 
-    @api.marshal_with(api.models.get('host_family'))
+    @api.marshal_with(api.models.get("host_family"))
     def post(self):
         return models.HostFamily(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('host_family'))
+    @api.marshal_with(api.models.get("host_family"))
     def put(self):
         data = models.HostFamily(**request.get_json()).save()
         return data.to_json()
 
-    @api.marshal_with(api.models.get('host_family'))
+    @api.marshal_with(api.models.get("host_family"))
     def patch(self):
         return models.HostFamily.set(**request.get_json()).to_json()
 
 
 @api.route("/host_family/<host_family_id>")
 class BaseHostFamilyController(Resource):
-
     def delete(self, host_family_id):
         return models.HostFamily.get(id=host_family_id).delete()
 
 
 @api.route("/host_family/base")
 class BaseHostFamilyController(Resource):
-
-    @api.marshal_list_with(api.models.get('host_family_reference'))
+    @api.marshal_list_with(api.models.get("host_family_reference"))
     def get(self):
         return [x.to_json(False) for x in models.HostFamily.get(**request.args)]
 
+
 @api.route("/account")
 class AccountController(Resource):
-
-    @api.marshal_list_with(api.models.get('account'), skip_none=True)
+    @api.marshal_list_with(api.models.get("account"), skip_none=True)
     def get(self):
         return models.Account.fetch(request.args)
 
-    @api.marshal_with(api.models.get('account'))
+    @api.marshal_with(api.models.get("account"))
     def post(self):
         return models.Account(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('account'))
+    @api.marshal_with(api.models.get("account"))
     def put(self):
         data = models.Account(**request.get_json()).save()
         return data.to_json()
 
-    @api.marshal_with(api.models.get('account'))
+    @api.marshal_with(api.models.get("account"))
     def patch(self):
         return models.Account.set(**request.get_json()).to_json()
 
 
 @api.route("/account/<account_id>")
 class BaseAccountController(Resource):
-
     def delete(self, account_id):
         return models.Account.get(id=account_id).delete()
 
 
 @api.route("/account/base")
 class BaseAccountController(Resource):
-
-    @api.marshal_list_with(api.models.get('account_reference'))
+    @api.marshal_list_with(api.models.get("account_reference"))
     def get(self):
         return [x.to_json(False) for x in models.Account.get(**request.args)]
 
+
 @api.route("/student_personal_data")
 class StudentPersonalDataController(Resource):
-
-    @api.marshal_list_with(api.models.get('student_personal_data'), skip_none=True)
+    @api.marshal_list_with(api.models.get("student_personal_data"), skip_none=True)
     def get(self):
         return models.StudentPersonalData.fetch(request.args)
 
-    @api.marshal_with(api.models.get('student_personal_data'))
+    @api.marshal_with(api.models.get("student_personal_data_reference"), skip_none=True)
     def post(self):
         return models.StudentPersonalData(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('student_personal_data'))
+    @api.marshal_with(api.models.get("student_personal_data_reference"), skip_none=True)
     def put(self):
-        data = models.StudentPersonalData(**request.get_json()).save()
-        return data.to_json()
+        return models.StudentPersonalData.update(**request.get_json()).to_json()
 
-    @api.marshal_with(api.models.get('student_personal_data'))
+    @api.marshal_with(api.models.get("student_personal_data_reference"), skip_none=True)
     def patch(self):
         return models.StudentPersonalData.set(**request.get_json()).to_json()
 
 
 @api.route("/student_personal_data/<student_personal_data_id>")
 class BaseStudentPersonalDataController(Resource):
+    @api.marshal_with(api.models.get("student_personal_data"), skip_none=True)
+    def get(self, student_personal_data_id):
+        return models.StudentPersonalData.objects.get(id=student_personal_data_id)
 
     def delete(self, student_personal_data_id):
         return models.StudentPersonalData.get(id=student_personal_data_id).delete()
@@ -261,8 +264,8 @@ class BaseStudentPersonalDataController(Resource):
 
 @api.route("/student_personal_data/base")
 class BaseStudentPersonalDataController(Resource):
-
-    @api.marshal_list_with(api.models.get('student_personal_data_reference'))
+    @api.marshal_list_with(api.models.get("student_personal_data_reference"))
     def get(self):
-        return [x.to_json(False) for x in models.StudentPersonalData.get(**request.args)]
-
+        return [
+            x.to_json(False) for x in models.StudentPersonalData.get(**request.args)
+        ]
--- models/__init__.py
+++ models/__init__.py
@@ -111,11 +111,11 @@ class Extended(Document):
                         setattr(self, key, kwargs[key])
 
                     # pass entire object
-                    elif "_id" in kwargs[key]:
+                    if "id" in kwargs[key]:
                         setattr(
                             self,
                             key,
-                            value.document_type_obj.objects.get(id=kwargs[key]["_id"]),
+                            value.document_type_obj.objects.get(id=kwargs[key]["id"]),
                         )
 
                     # pass ObjectId string of object
@@ -131,7 +131,7 @@ class Extended(Document):
                         setattr(
                             self,
                             key,
-                            value.document_type_obj(**{key: {"_id": kwargs[key]}}),
+                            value.document_type_obj(**{key: {"id": kwargs[key]}}),
                         )
 
                 # special for Raw fields that are wildcards
@@ -149,21 +149,28 @@ class Extended(Document):
             else:
                 return v
 
-        return {**{k: f(v) for k, v in loads(json_util.dumps(self.to_mongo())).items()}}
+        data = {**{k: f(v) for k, v in loads(json_util.dumps(self.to_mongo())).items()}}
+        data.update({"id": data.pop("_id")})
+
+        return data
 
     @classmethod
     def set(cls, *args, **kwargs):
-        _id = kwargs.pop("id")
-        item = cls.objects.get(id=_id)
+        id = kwargs.pop("id")
+        item = cls.objects.get(id=id)
 
         # ???
         for key, value in kwargs.items():
+
             if isinstance(value, list) and any(value):
-                string_object_ids = [x.get("_id", {}).get("$oid") for x in value]
+                string_object_ids = [x.get("id", {}).get("$oid") for x in value]
                 if all(map(lambda x: ObjectId.is_valid(x), string_object_ids)):
                     value = [ObjectId(x) for x in string_object_ids]
 
-        cls.objects(id=_id).update_one(
+            elif isinstance(value, dict) and "id" in value:
+                kwargs[key] = value["id"]
+
+        cls.objects(id=id).update_one(
             **{
                 key.replace("$", "")
                 if key.startswith("$")
@@ -174,7 +181,38 @@ class Extended(Document):
             }
         )
 
-        return cls.objects.get(id=_id)
+        return cls.objects.get(id=id)
+
+    @classmethod
+    def update(cls, *args, **kwargs):
+        id = kwargs.pop("id")
+        item = cls.objects.get(id=id)
+
+        # ???
+        for key, value in kwargs.items():
+            if isinstance(value, list) and any(value):
+                string_object_ids = [x.get("id", {}).get("$oid") for x in value]
+                if all(map(lambda x: ObjectId.is_valid(x), string_object_ids)):
+                    value = [ObjectId(x) for x in string_object_ids]
+
+            elif isinstance(value, dict) and "id" in value:
+                kwargs[key] = value["id"]
+
+        [delattr(item, k) for k, v in cls._fields.items() if k not in ["_cls", "id"]]
+
+        item.save()
+
+        cls.objects(id=id).update_one(
+            **{
+                key.replace("$", "")
+                if key.startswith("$")
+                else "set__{}".format(key): (
+                    ObjectId(value) if ObjectId.is_valid(value) else value
+                )
+                for key, value in kwargs.items()
+            }
+        )
+        return cls.objects.get(id=id)
 
     @classmethod
     def get(cls, *args, **kwargs):
@@ -247,7 +285,7 @@ class Extended(Document):
                 for key, value in list(cls._fields.items())
                 if hasattr(getattr(cls, key), "marshal")
             },
-            "_id": String(),
+            "id": String(),
         }
 
     @classmethod
@@ -255,7 +293,7 @@ class Extended(Document):
         return {
             **cls.base(),
             **{
-                field: String(attribute=lambda x: x["_id"])
+                field: String()
                 for field, instance in cls._fields.items()
                 if isinstance(instance, ReferenceField)
             },
@@ -323,18 +361,19 @@ class Extended(Document):
                         "id__in": ids,
                     },
                 )
-                values = {x["_id"]: x for x in values}
+
+                # values = {x["_id"]: x for x in values}
 
                 for item in data:
                     item.update({key: [values[x] for x in item[key]]})
 
             else:
                 for item in data:
-                    item.update({key: [{"_id": x} for x in item[key]]})
+                    item.update({key: [{"id": x} for x in item[key]]})
 
         for key, value in cls._reference_fields().items():
             if key in include:
-                ids = list(map(lambda x: x[key], data))
+                ids = [x.get(key) for x in data if key in x]
                 values = value.document_type_obj.fetch(
                     {
                         "$include": ",".join(
@@ -347,15 +386,18 @@ class Extended(Document):
                         "id__in": ids,
                     },
                 )
-                values = {x["_id"]: x for x in values}
 
-                list(map(lambda x: x.update({key: values[x[key]]}), data))
+                values = {x["id"]: x for x in values}
+
+                for item in data:
+                    if key in item and item[key] in values:
+                        item[key] = values[item[key]]
 
             else:
                 list(
                     map(
                         lambda x: x.update(
-                            {key: ({"_id": x.get(key)} if key in x else None)}
+                            {key: ({"id": x.get(key)} if key in x else None)}
                         ),
                         data,
                     )
@@ -372,13 +414,13 @@ class Program(Extended):
     price = FloatField()
 
 
-class AirportCode(Extended):
-    airport = StringField()
+class Airport(Extended):
+    name = StringField()
     code = StringField()
 
 
 class FlightInfo(Extended):
-    airport_code = ReferenceField(AirportCode, reverse_delete_rule=NULLIFY)
+    airport = ReferenceField(Airport, reverse_delete_rule=NULLIFY)
     depart_date = DateTimeField()
     depart_time = StringField()
     depart_flight_number = StringField()
@@ -401,7 +443,7 @@ class HostFamily(Extended):
     email = StringField()
     phone_extension = StringField()
     phone_number = StringField()
-    airport_code = ReferenceField(AirportCode, reverse_delete_rule=NULLIFY)
+    airport = ReferenceField(Airport, reverse_delete_rule=NULLIFY)
     profile_link = StringField()
 
 
@@ -428,7 +470,7 @@ class StudentPersonalData(Extended):
     gender = StringField()
     email = StringField()
     program = ReferenceField(Program, reverse_delete_rule=NULLIFY)
-    airport_code = ReferenceField(AirportCode, reverse_delete_rule=NULLIFY)
+    airport = ReferenceField(Airport, reverse_delete_rule=NULLIFY)
     host_family = ReferenceField(HostFamily, reverse_delete_rule=NULLIFY)
     date_of_application = DateTimeField()
     date_of_birth = DateTimeField()
@@ -443,25 +485,40 @@ class StudentPersonalData(Extended):
     school_name = StringField()
     school_type = StringField()
     average_grades = StringField()
-
+    father_first_name = StringField()
+    father_last_name = StringField()
+    father_address_line_1 = StringField()
+    father_address_line_2 = StringField()
+    father_address_city = StringField()
+    father_address_postal_code = StringField()
+    father_address_country = StringField()
+    mother_first_name = StringField()
+    mother_last_name = StringField()
+    mother_address_line_1 = StringField()
+    mother_address_line_2 = StringField()
+    mother_address_city = StringField()
+    mother_address_postal_code = StringField()
+    mother_address_country = StringField()
+    allergies = StringField()
+    interview = DateTimeField()
 
 
 # def config():
-    # signals.pre_save.connect(Class.pre_save, sender=Class)
-    # signals.post_save.connect(Class.post_save, sender=Class)
+# signals.pre_save.connect(Class.pre_save, sender=Class)
+# signals.post_save.connect(Class.post_save, sender=Class)
 
-    # seed
-    # logging.info("Seeding database")
-    # seed = load(open("models/seed.json"))
+# seed
+# logging.info("Seeding database")
+# seed = load(open("models/seed.json"))
 
-    # helper method to remove "_id" and "_cls" so I can compare json objects
-    # from the db
-    # def remove_meta_from_dict_item(item):
-    #     item.pop("_cls")
-    #     item.pop("_id")
-    #     for key, value in item.items():
-    #         if isinstance(value, dict):
-    #             remove_meta_from_dict_item(value)
+# helper method to remove "_id" and "_cls" so I can compare json objects
+# from the db
+# def remove_meta_from_dict_item(item):
+#     item.pop("_cls")
+#     item.pop("_id")
+#     for key, value in item.items():
+#         if isinstance(value, dict):
+#             remove_meta_from_dict_item(value)
 
 
 # config()
