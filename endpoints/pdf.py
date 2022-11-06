795db99d674f07e7a79a05f62bf68886e556e9b9
isebarn182@gmail.com <isebarn182@gmail.com>
2022-11-06 13:06:21 +0000

    asd

--- endpoints/pdf.py
+++ endpoints/pdf.py
@@ -22,36 +22,39 @@ api = Namespace("pdf", description="")
 class PDFController(_Resource):
     def get(self, student_id):
 
-        method = request.args.get("method")
-        student_profile = StudentProfile.objects.get(id=student_id).to_json()
-        student_personal_data = StudentPersonalData.objects.get(student_profile=student_profile['id']).to_json()
-        programme = Program.objects.get(id=student_profile.get('program')).to_json()
-        name = student_profile.get("first_name", "")
-        address = student_personal_data.get("address")
+        try:
+            method = request.args.get("method")
+            student_profile = StudentProfile.objects.get(id=student_id).to_json()
+            student_personal_data = StudentPersonalData.objects.get(student_profile=student_profile['id']).to_json()
+            programme = Program.objects.get(id=student_profile.get('program')).to_json()
+            name = student_profile.get("first_name", "")
+            address = student_personal_data.get("address")
 
-        student_personal_data['parentsname'] = ""
-        if "father" in student_personal_data:
-            student_personal_data['parentsname'] += f'Mr {student_personal_data.get("father", {}).get("first_name", "")}'
+            student_personal_data['parentsname'] = ""
+            if "father" in student_personal_data:
+                student_personal_data['parentsname'] += f'Mr {student_personal_data.get("father", {}).get("first_name", "")}'
 
-        if "mother" in student_personal_data:
-            student_personal_data['parentsname'] += f', Mrs {student_personal_data.get("mother", {}).get("first_name", "")}'
+            if "mother" in student_personal_data:
+                student_personal_data['parentsname'] += f', Mrs {student_personal_data.get("mother", {}).get("first_name", "")}'
 
-        elif "mother" in student_personal_data:
-            student_personal_data['parentsname'] =+ f'Mrs {student_personal_data.get("mother", {}).get("first_name", "")}'
+            elif "mother" in student_personal_data:
+                student_personal_data['parentsname'] =+ f'Mrs {student_personal_data.get("mother", {}).get("first_name", "")}'
 
-        student_personal_data.update({
-            "name": f'{student_profile.get("first_name", "")} {student_profile.get("last_name", "")}',
-            "address": f'{address.get("line_1", "")}, {address.get("city", "")}, {address.get("country", "")}',
-            "first_name": student_profile.get("first_name", "")     
-        })
+            student_personal_data.update({
+                "name": f'{student_profile.get("first_name", "")} {student_profile.get("last_name", "")}',
+                "address": f'{address.get("line_1", "")}, {address.get("city", "")}, {address.get("country", "")}',
+                "first_name": student_profile.get("first_name", "")     
+            })
 
-        programme.update({
-            "code": programme.get("code", ""),
-            "country": programme.get("country", ""),
-            "description": programme.get("description", ""),
-            "price": programme.get("price", 0),
-        })
-
+            programme.update({
+                "code": programme.get("code", ""),
+                "country": programme.get("country", ""),
+                "description": programme.get("description", ""),
+                "price": programme.get("price", 0),
+            })
+        except Exception as e:
+            pass
+            
         if method == "generate_thank_you_letter": 
             generate_thank_you_letter(student_id, student_personal_data, programme)
 
